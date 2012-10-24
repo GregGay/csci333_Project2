1babfb4f48f74699046d253b6d0ef7f2f2158006
greggay <greggay@debian.myhome.westell.com>
2012-10-23 23:26:25 -0400

    Vector of Vector 2DArray header file

---
+++ src/Vector/VectorTwoDArray.h
@@ -0,0 +1,25 @@
+#ifndef __VECTORTWODARRAY_H__
+#define __VECTORTWODARRAY_H__
+
+#include <vector>
+
+template <typename T>
+class VectorTwoDArray {
+    private:
+	  std::vector< std::vector<T> > theArray;
+	  int row;
+	  int col;
+	  T Def;
+
+    public:
+	  VectorTwoDArray<T>(int r, int c, T def);
+	  ~VectorTwoDArray<T>();
+	  void insert(int r, int c, T value);
+	  T access(int r, int c);
+	  void remove(int r, int c);
+	  void print();
+	  int getNumRows();
+	  int getNumCols();
+};
+
+#endif
