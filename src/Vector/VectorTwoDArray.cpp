ce5cd263e83cb4ddd921474d322eb709d24afe23
greggay <greggay@debian.myhome.westell.com>
2012-10-23 23:26:12 -0400

    Vector of Vector 2DArray implementation

---
+++ src/Vector/VectorTwoDArray.cpp
@@ -0,0 +1,95 @@
+#include "VectorTwoDArray.h"
+#include "assert.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+
+template <typename T>
+VectorTwoDArray<T>::VectorTwoDArray(int r, int c, T def) {
+    row = r;
+    col = c;
+    Def = def;
+
+    for (int i = 0; i < r; i++) {
+	  std::vector<T> rows;
+	  for (int j = 0; j < c; j++) {
+		rows.push_back(i * j);
+	  }
+	  theArray.push_back(rows);
+    }
+
+    /*
+    theArray = new T[r];
+
+    for(int i = 0; i < r; ++i) {
+	  theArray[i] = new T[c];
+    }
+
+    for(int i = 0; i < row; i++) {
+	  for (int j = 0; j < col; j++) {
+		theArray[i][j] = Def;
+	  }
+    }*/
+}
+
+template <typename T>
+VectorTwoDArray<T>::~VectorTwoDArray() {
+/*    for(int i = 0; i < row; i++) {
+	  delete[] theArray[i];
+    }
+
+    delete[] theArray;
+*/
+}
+
+template <typename T>
+void VectorTwoDArray<T>::insert(int r, int c, T value) {
+    assert(r >= 0 && r < row);
+    assert(c >= 0 && c < col);
+
+    theArray[r][c] = value;
+}
+
+template <typename T>
+T VectorTwoDArray<T>::access(int r, int c) {
+    assert(r >= 0 && r < row);
+    assert(c >= 0 && c < col);
+    
+    return theArray[r][c];
+}
+
+template <typename T>
+void VectorTwoDArray<T>::remove(int r, int c) {
+    assert(r >= 0 && r < row);
+    assert(c >= 0 && c < col);
+    
+    theArray[r][c] = Def;
+}
+
+template <typename T>
+void VectorTwoDArray<T>::print() {
+    cout << "[ ";
+    for(int i = 0; i < row; i++) {
+	  for(int j = 0; j < col; j++) {
+		cout << theArray[i][j] << " ";
+	  }
+    }
+    cout << "]" << endl;
+}
+
+template <typename T>
+int VectorTwoDArray<T>::getNumRows() {
+    return row;
+}
+
+template <typename T>
+int VectorTwoDArray<T>::getNumCols() {
+    return col;
+}
+
+template class VectorTwoDArray<int>;
+template class VectorTwoDArray<double>;
+//template class VectorTwoDArray<std::string>;
