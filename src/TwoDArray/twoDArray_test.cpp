5ca77a5f9c9585e65d1def4569c703d48242584e
greggay <greggay@debian.myhome.westell.com>
2012-10-26 23:09:36 -0400

    TwoDArray directory

---
+++ src/TwoDArray/twoDArray_test.cpp
@@ -0,0 +1,50 @@
+#include "TwoDArray.h"
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    TwoDArray<int>* twoDInt = new TwoDArray<int>(2, 2, 0);
+    
+    cout << "Integer insert: " << endl;
+    twoDInt->insert(1, 0, 7);
+    twoDInt->print();
+    
+    cout << "Integer access: " << twoDInt->access(1, 0) << endl;
+    
+    cout << "Integer remove: " << endl;
+    twoDInt->remove(1, 0);
+    twoDInt->print();
+    
+    TwoDArray<double>* twoDDouble = new TwoDArray<double>(2, 2, 0.01);
+    
+    cout << "Double insert: " << endl;
+    twoDDouble->insert(1, 0, 7.77);
+    twoDDouble->print();
+    
+    cout << "Double access: " << twoDDouble->access(1, 0) << endl;
+
+    cout << "Double remove: " << endl;
+    twoDDouble->remove(1, 0);
+    twoDDouble->print();
+    
+    TwoDArray<std::string>* twoDString = new TwoDArray<std::string>(2, 2, "default");
+    
+    cout << "String insert: " << endl;
+    twoDString->insert(1, 0, "Hello");
+    twoDString->print();
+    
+    cout << "String insert: " << endl;
+    twoDString->insert(0, 1, "World");
+    twoDString->print();
+    
+    cout << "String access: " << twoDString->access(1, 0) << endl;
+    
+    cout << "String remove: " << endl;
+    twoDString->remove(1, 0);
+    twoDString->print();
+
+    return 0;
+}
